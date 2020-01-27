2d4ab83398a146497dadd15ab252562650d07e94
Karin Hendrikse <30577427+khendrikse@users.noreply.github.com>
2020-01-27 09:06:21 +0100

    Update blue_purple.py

--- blue_purple.py
+++ blue_purple.py
@@ -12,16 +12,16 @@ blinkt.set_clear_on_exit()
 def set_brightness(pixel, r, g, b, brightRange):
     for brightness in brightRange:
         blinkt.set_pixel(pixel, r, g, b, brightness)
-	blinkt.show()
-	time.sleep(0.01)
+        blinkt.show()
+        time.sleep(0.01)
 
 try:       
     while True:
 	for color in COLOR_LIST:
-	    for pixel in range(blinkt.NUM_PIXELS):
-		set_brightness(pixel, color[0], color[1], color[2], BRIGHTNESS_RANGE)
-	    for pixel in reversed(range(blinkt.NUM_PIXELS)):
-		set_brightness(pixel, color[0], color[1], color[2], reversed(BRIGHTNESS_RANGE))
+            for pixel in range(blinkt.NUM_PIXELS):
+                set_brightness(pixel, color[0], color[1], color[2], BRIGHTNESS_RANGE)
+            for pixel in reversed(range(blinkt.NUM_PIXELS)):
+                set_brightness(pixel, color[0], color[1], color[2], reversed(BRIGHTNESS_RANGE))
 except KeyboardInterrupt:
     pass
 
