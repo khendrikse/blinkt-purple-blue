766c4fec483c7c37c36a7111e2c837ecb615e4e9
khendrikse <30577427+khendrikse@users.noreply.github.com>
2020-01-27 09:42:49 +0100

    add indentation

--- blue_purple.py
+++ blue_purple.py
@@ -4,10 +4,12 @@ import blinkt
 import time
 import numpy
 
-COLOR_LIST = [[0, 127, 255], [0, 0, 255], [127, 0, 255], [255, 0, 255], [255, 0, 127]]
+COLOR_LIST = [[0, 127, 255], [0, 0, 255], [
+    127, 0, 255], [255, 0, 255], [255, 0, 127]]
 BRIGHTNESS_RANGE = numpy.arange(0, 0.5, 0.03)
 
 blinkt.set_clear_on_exit()
+
 
 def set_brightness(pixel, r, g, b, brightRange):
     for brightness in brightRange:
@@ -15,13 +17,16 @@ def set_brightness(pixel, r, g, b, brightRange):
         blinkt.show()
         time.sleep(0.01)
 
-try:       
+
+try:
     while True:
-	for color in COLOR_LIST:
+        for color in COLOR_LIST:
             for pixel in range(blinkt.NUM_PIXELS):
-                set_brightness(pixel, color[0], color[1], color[2], BRIGHTNESS_RANGE)
+                set_brightness(
+                    pixel, color[0], color[1], color[2], BRIGHTNESS_RANGE)
             for pixel in reversed(range(blinkt.NUM_PIXELS)):
-                set_brightness(pixel, color[0], color[1], color[2], reversed(BRIGHTNESS_RANGE))
+                set_brightness(
+                    pixel, color[0], color[1], color[2], reversed(BRIGHTNESS_RANGE))
 except KeyboardInterrupt:
     pass
 
